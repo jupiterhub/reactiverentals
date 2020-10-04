f3225b223d3cb0110e1796c66c58c8c8c112feef
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-10-05 06:55:48 +0900

    feat: added router for person endpoins

--- src/main/java/org/jupiterhub/reactiverentals/routerfunction/PersonRouter.java
+++ src/main/java/org/jupiterhub/reactiverentals/routerfunction/PersonRouter.java
@@ -1,16 +1,21 @@
 package org.jupiterhub.reactiverentals.routerfunction;
 
-import org.springframework.http.MediaType;
+import org.jupiterhub.reactiverentals.handler.PersonHandler;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
-public class HelloWorldRouter {
+public class PersonRouter {
+
+    private PersonHandler handler;
 
     RouterFunction<ServerResponse> route = RouterFunctions.route()
-            .GET("/hello-world", accept(MediaType.TEXT_PLAIN),
-                    request -> ServerResponse.ok().bodyValue("Hello World")).build();
+            .GET("/person/{id}", accept(APPLICATION_JSON), handler::getPerson)
+            .GET("/person", accept(APPLICATION_JSON), handler::listPeople)
+            .POST("/person", handler::createPerson)
+            .build();
 
 }
