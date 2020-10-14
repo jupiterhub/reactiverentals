1d55bc09ee44d938968d6c6095f81a48a09a521b
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-10-14 18:08:56 +0900

    refactor: use component based handlers instead of defining in webconfig

--- src/main/java/org/jupiterhub/reactiverentals/WebConfig.java
+++ src/main/java/org/jupiterhub/reactiverentals/WebConfig.java
@@ -1,28 +1,19 @@
 package org.jupiterhub.reactiverentals;
 
-import org.springframework.context.annotation.Bean;
+import org.jupiterhub.reactiverentals.router.PersonRouter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.ViewResolverRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.function.server.RouterFunction;
 
 @Configuration
 @EnableWebFlux
 public class WebConfig implements WebFluxConfigurer {
-    @Bean
-    public RouterFunction<?> routerFunctionA() {
-        // TODO
-        return null;
-    }
-
-    @Bean
-    public RouterFunction<?> routerFunctionB() {
-        // TODO
-        return null;
-    }
+    @Autowired
+    private PersonRouter personRouter;  // this will be automatically scanned and can possible be removed. Alternatively declare as @Bean
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/org/jupiterhub/reactiverentals/router/PersonRouter.java
+++ src/main/java/org/jupiterhub/reactiverentals/router/PersonRouter.java
@@ -1,6 +1,7 @@
 package org.jupiterhub.reactiverentals.router;
 
 import org.jupiterhub.reactiverentals.handler.PersonHandler;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -8,14 +9,17 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
+@Component
 public class PersonRouter {
 
     private PersonHandler handler;
 
-    RouterFunction<ServerResponse> route = RouterFunctions.route()
-            .GET("/person/{id}", accept(APPLICATION_JSON), handler::getPerson)
-            .GET("/person", accept(APPLICATION_JSON), handler::listPeople)
-            .POST("/person", handler::createPerson)
-            .build();
+    public RouterFunction<ServerResponse> getPersonRoutes() {
+        return RouterFunctions.route()
+                .GET("/person/{id}", accept(APPLICATION_JSON), handler::getPerson)
+                .GET("/person", accept(APPLICATION_JSON), handler::listPeople)
+                .POST("/person", handler::createPerson)
+                .build();
+    }
 
 }
