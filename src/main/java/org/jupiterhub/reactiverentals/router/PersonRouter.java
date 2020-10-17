fdc68193f1a221db11bb39d958fa2de25f404d72
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-10-17 23:40:36 +0900

    feat: added sample for filtered routers

--- src/main/java/org/jupiterhub/reactiverentals/router/PersonRouter.java
+++ src/main/java/org/jupiterhub/reactiverentals/router/PersonRouter.java
@@ -2,6 +2,9 @@ package org.jupiterhub.reactiverentals.router;
 
 import org.jupiterhub.reactiverentals.handler.PersonHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -15,6 +18,8 @@ public class PersonRouter {
 
     @Autowired
     private PersonHandler handler;
+
+    private SecurityExpressionRoot securityManager;
 
     public RouterFunction<ServerResponse> getPersonRoutes() {
         return RouterFunctions.route()
@@ -31,6 +36,25 @@ public class PersonRouter {
                                 .GET("/{id}", handler::getPerson)
                                 .GET("", handler::listPeople))
                         .POST("/person", handler::createPerson))
+                .build();
+    }
+
+    public RouterFunction<ServerResponse> getPersonRoutesWitSecurity() {
+        return RouterFunctions.route()
+                .path("/person", b1 -> b1
+                        .nest(accept(APPLICATION_JSON), b2 -> b2
+                                .GET("/{id}", handler::getPerson)
+                                .GET("", handler::listPeople))
+                        .POST("/person", handler::createPerson))
+                .filter((request, next) -> {
+                    // sample filter on
+                    if (securityManager.hasRole(request.headers().firstHeader(HttpHeaders.AUTHORIZATION))) {
+                        return next.handle(request);
+                    }
+                    else {
+                        return ServerResponse.status(HttpStatus.UNAUTHORIZED).build();
+                    }
+                })
                 .build();
     }
 
