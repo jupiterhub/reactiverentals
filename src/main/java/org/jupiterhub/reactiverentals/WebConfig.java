f6edf7c78ff750a097f10c34965a088868e2667a
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-10-18 14:23:12 +0900

    feat: added cors support

--- src/main/java/org/jupiterhub/reactiverentals/WebConfig.java
+++ src/main/java/org/jupiterhub/reactiverentals/WebConfig.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.ViewResolverRegistry;
@@ -24,9 +27,33 @@ public class WebConfig implements WebFluxConfigurer {
         return personRouter.getPersonRoutes();
     }
 
+    @Bean
+    CorsWebFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.applyPermitDefaultValues();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://domain1.com");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
+        // For MVC support this is a more traditional way, see #corsFilter for functional endpoint appriach
+//        registry.addMapping("/person/**")
+//                .allowedOrigins("https://rental-api.com")
+//                .allowedMethods("POST", "DELETE")
+//                .allowedHeaders("header1", "header2", "header3")
+//                .exposedHeaders("header1", "header2")
+//                .allowCredentials(true).maxAge(3600);
     }
 
     @Override
