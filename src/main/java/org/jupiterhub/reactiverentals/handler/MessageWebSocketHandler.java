ec1e90f3bb843bf0875a1e6f2c7b285cfe6f5b34
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-10-19 20:17:00 +0900

    feat: addedsample for websocket handler

--- src/main/java/org/jupiterhub/reactiverentals/handler/MessageWebSocketHandler.java
+++ src/main/java/org/jupiterhub/reactiverentals/handler/MessageWebSocketHandler.java
@@ -1,13 +1,23 @@
 package org.jupiterhub.reactiverentals.handler;
 
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class MessageWebSocketHandler implements WebSocketHandler {
     @Override
     public Mono<Void> handle(WebSocketSession session) {
-        // TODO
-        return Mono.empty();
+        Flux<WebSocketMessage> output = session.receive()
+                .doOnNext(message -> {
+                    // ...
+                })
+                .concatMap(message -> {
+                    return Flux.empty();
+                })
+                .map(value -> session.textMessage("Echo " + value));
+
+        return session.send(output);
     }
 }
