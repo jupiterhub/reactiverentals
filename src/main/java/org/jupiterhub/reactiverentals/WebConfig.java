a06182f050a3d24f818dc5427656b30ab1aa5ce7
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-10-19 12:11:18 +0900

    feat: added script templating support

--- src/main/java/org/jupiterhub/reactiverentals/WebConfig.java
+++ src/main/java/org/jupiterhub/reactiverentals/WebConfig.java
@@ -4,6 +4,8 @@ import org.jupiterhub.reactiverentals.router.PersonRouter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
@@ -14,6 +16,7 @@ import org.springframework.web.reactive.config.ViewResolverRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.script.ScriptTemplateConfigurer;
 
 @Configuration
 @EnableWebFlux
@@ -57,12 +60,31 @@ public class WebConfig implements WebFluxConfigurer {
     }
 
     @Override
-    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+    public void addFormatters(FormatterRegistry registry) {
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setUseIsoFormat(true);
+        registrar.registerFormatters(registry);
+    }
 
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        // default is 256kb
+        configurer.defaultCodecs().maxInMemorySize(512 * 1024);
     }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
+        // use react as script
+        registry.scriptTemplate();
+    }
 
+    @Bean
+    public ScriptTemplateConfigurer configurer() {
+        ScriptTemplateConfigurer configurer = new ScriptTemplateConfigurer();
+        configurer.setEngineName("nashorn");
+        configurer.setScripts("polyfill.js", "react.js", "render.js");
+        configurer.setRenderFunction("render");
+        configurer.setSharedEngine(false);
+        return configurer;
     }
 }
