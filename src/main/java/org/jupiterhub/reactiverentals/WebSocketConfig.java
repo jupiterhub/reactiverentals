a16d7b9530a6d6545695fc6ef3bc4bf43ecb5fd6
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-10-20 23:26:21 +0900

    feat: set idel session timeout to 0

--- src/main/java/org/jupiterhub/reactiverentals/WebSocketConfig.java
+++ src/main/java/org/jupiterhub/reactiverentals/WebSocketConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.WebSocketService;
+import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import org.springframework.web.reactive.socket.server.upgrade.TomcatRequestUpgradeStrategy;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +28,13 @@ public class WebSocketConfig {
 
     @Bean
     public WebSocketHandlerAdapter handlerAdapter() {
-        return new WebSocketHandlerAdapter();
+        return new WebSocketHandlerAdapter(webSocketService());
+    }
+
+    @Bean
+    public WebSocketService webSocketService() {
+        TomcatRequestUpgradeStrategy strategy = new TomcatRequestUpgradeStrategy();
+        strategy.setMaxSessionIdleTimeout(0L);
+        return new HandshakeWebSocketService(strategy);
     }
 }
