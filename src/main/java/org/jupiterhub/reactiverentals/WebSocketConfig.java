66c66d1ecf469c86e4ae24b3b38842cde0ec5a18
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-10-22 20:14:00 +0900

    build: add spring-test deps

--- src/main/java/org/jupiterhub/reactiverentals/WebSocketConfig.java
+++ src/main/java/org/jupiterhub/reactiverentals/WebSocketConfig.java
@@ -1,55 +1,53 @@
-package org.jupiterhub.reactiverentals;
-
-import org.jupiterhub.reactiverentals.handler.MessageWebSocketHandler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.HandlerMapping;
-import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
-import org.springframework.web.reactive.socket.client.WebSocketClient;
-import org.springframework.web.reactive.socket.server.WebSocketService;
-import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
-import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
-import org.springframework.web.reactive.socket.server.upgrade.TomcatRequestUpgradeStrategy;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-public class WebSocketConfig {
-
-    @Bean
-    public HandlerMapping handlerMapping() {
-        Map<String, WebSocketHandler> map = new HashMap<>();
-        map.put("/messsage", new MessageWebSocketHandler());
-        int order = -1; // before annotated controllers
-
-        return new SimpleUrlHandlerMapping(map, order);
-    }
-
-//    @Bean
-    public WebSocketHandlerAdapter handlerAdapter() {
-        return new WebSocketHandlerAdapter(webSocketService());
-    }
-
-    @Bean
-    public WebSocketService webSocketService() {
-        TomcatRequestUpgradeStrategy strategy = new TomcatRequestUpgradeStrategy();
-        strategy.setMaxSessionIdleTimeout(0L);
-        return new HandshakeWebSocketService(strategy);
-    }
-
-
-    public void connectByWebSocket() throws URISyntaxException {
-        WebSocketClient client = new ReactorNettyWebSocketClient();
-
-        URI url = new URI("ws://localhost:8080/path");
-        client.execute(url, session ->
-                session.receive()
-                        .doOnNext(System.out::println)
-                        .then());
-    }
-}
+//package org.jupiterhub.reactiverentals;
+//
+//import org.jupiterhub.reactiverentals.handler.MessageWebSocketHandler;
+//import org.springframework.web.reactive.HandlerMapping;
+//import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+//import org.springframework.web.reactive.socket.WebSocketHandler;
+//import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+//import org.springframework.web.reactive.socket.client.WebSocketClient;
+//import org.springframework.web.reactive.socket.server.WebSocketService;
+//import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
+//import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+//import org.springframework.web.reactive.socket.server.upgrade.TomcatRequestUpgradeStrategy;
+//
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+////@Configuration
+//public class WebSocketConfig {
+//
+////    @Bean
+//    public HandlerMapping handlerMapping() {
+//        Map<String, WebSocketHandler> map = new HashMap<>();
+//        map.put("/messsage", new MessageWebSocketHandler());
+//        int order = -1; // before annotated controllers
+//
+//        return new SimpleUrlHandlerMapping(map, order);
+//    }
+//
+////    @Bean
+//    public WebSocketHandlerAdapter handlerAdapter() {
+//        return new WebSocketHandlerAdapter(webSocketService());
+//    }
+//
+////    @Bean
+//    public WebSocketService webSocketService() {
+//        TomcatRequestUpgradeStrategy strategy = new TomcatRequestUpgradeStrategy();
+//        strategy.setMaxSessionIdleTimeout(0L);
+//        return new HandshakeWebSocketService(strategy);
+//    }
+//
+//
+//    public void connectByWebSocket() throws URISyntaxException {
+//        WebSocketClient client = new ReactorNettyWebSocketClient();
+//
+//        URI url = new URI("ws://localhost:8080/path");
+//        client.execute(url, session ->
+//                session.receive()
+//                        .doOnNext(System.out::println)
+//                        .then());
+//    }
+//}
