160b1b67b37c568458cbf5c7e04fbd6be19c650a
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-11-01 10:01:03 +0900

    feat: property registration, add domain

---
+++ src/main/java/org/jupiterhub/reactiverentals/record/Property.java
@@ -0,0 +1,6 @@
+package org.jupiterhub.reactiverentals.record;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+public record Account(BigInteger accountId, String emailAddress, String accountName, Date registrationDate) {}
