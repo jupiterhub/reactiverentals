b967062fb8b5d6457b6ade0cabb93199d3de4ced
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2020-11-05 23:01:04 +0900

    feat: contract agreement, add behavior score for user

--- src/main/java/org/jupiterhub/reactiverentals/record/Account.java
+++ src/main/java/org/jupiterhub/reactiverentals/record/Account.java
@@ -3,4 +3,4 @@ package org.jupiterhub.reactiverentals.record;
 import java.math.BigInteger;
 import java.util.Date;
 
-public record Account(BigInteger accountId, String emailAddress, String accountName, Date registrationDate) {}
+public record Account(BigInteger accountId, String emailAddress, String accountName, Date registrationDate, Integer behaviorScore) {}
